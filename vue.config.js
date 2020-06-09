7155f0ebaca7d85cfaf7e8a22fa4020d67e7c7e4
kahoo huang <454191479@qq.com>
2020-06-09 23:26:22 +0800

    commits

--- vue.config.js
+++ vue.config.js
@@ -16,17 +16,3 @@ module.exports = {
   },
 }
 
-// module.exports = {
-//   configureWebpack: {
-//     resolve: {
-//       alias: {
-//         'components': '@/components',
-//         'content': 'components/content',
-//         'common': 'components/common',
-//         'assets': '@/assets',
-//         'network': '@/network',
-//         'views': '@/views',
-//       }
-//     }
-//   }
-// }
